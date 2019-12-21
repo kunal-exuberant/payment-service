0f8b9b1fa93128984a98cd66841d8eabbc30887c
Kunal Singh1 <kunal.singh1@localadmins-MacBook-Pro.local>
2019-12-21 17:51:41 +0530

    bootstrap

---
+++ src/main/java/transaction/models/SenderTransactionState.java
@@ -0,0 +1,11 @@
+package transaction.models;
+
+public enum  SenderTransactionState {
+    SENT,
+    ACK_RECEIVED,
+    COMMIT_REQUEST,
+    COMMIT_RECEIVED,
+    SENDER_COMMIT,
+    ROLLBACK_REQUEST,
+    ROLLBACK_DONE
+}
